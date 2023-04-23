fac7a9a609b6e170fa7c7e4209cde09f1c9b3e5d
Pikman1210 <72507796+Pikman1210@users.noreply.github.com>
2023-04-23 13:55:07 -0500

    Update WallTeleporter.cs

--- Assets/Scripts/WallTeleporter.cs
+++ Assets/Scripts/WallTeleporter.cs
@@ -5,9 +5,6 @@ public class WallTeleporter : MonoBehaviour {
     public GameObject player;
 
     [SerializeField]
-    private int DestinationIndex;
-
-    [SerializeField]
     private Vector3 minPosition;
     
     [SerializeField]
@@ -21,7 +18,7 @@ public class WallTeleporter : MonoBehaviour {
         }
     }
 
-    public void Teleport()//(int destination)
+    public void Teleport()
     {
         Vector3 randomPosition = new Vector3(
             Random.Range(minPosition.x, maxPosition.x),
@@ -30,28 +27,6 @@ public class WallTeleporter : MonoBehaviour {
         );
 
         player.transform.localPosition = randomPosition;
-
-        /*switch (destination)
-        {
-            case 0:
-                player.transform.localPosition = new Vector3(0, 0, 0);
-                break;
-            case 1:
-                player.transform.localPosition = new Vector3(56, 0, 56);
-                break;
-            case 2:
-                player.transform.localPosition = new Vector3(-23, 0, -46);
-                break;
-            case 3:
-                player.transform.localPosition = new Vector3(-18, 0, 66);
-                break;
-            case 4:
-                player.transform.localPosition = new Vector3(32, 0, -11);
-                break;
-            default:
-                Debug.LogWarning("Wall teleport failed, uh oh");
-                break;
-        } */
 
         Physics.SyncTransforms(); // Super important, actually makes the transform happen!
     }
