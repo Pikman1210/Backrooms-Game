e0e25dbbc5060100e54d588901ad95d8a0474291
Pikman1210 <72507796+Pikman1210@users.noreply.github.com>
2023-04-23 16:53:41 -0500

    Update EscapeDoor.cs

--- Assets/Scripts/EscapeDoor.cs
+++ Assets/Scripts/EscapeDoor.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Escape : MonoBehaviour
+public class EscapeDoor : MonoBehaviour
 {
+
+    [SerializeField]
+    private Vector3[] spawnPoints;
+
+    [SerializeField]
+    [Tooltip("Corresponds to the respective element number of spawn point")]
+    private Quaternion[] rotationSets; // Replace with if statement where if index _ (ones that need rotation)
+    // then rotate by 90 degrees  (0, 3, 6, 8, 9, 18)
+    // these need 270 degrees (5, 19)
+    // these need 180 (10, 12, 13, 17)
 
     private void Start()
     {
