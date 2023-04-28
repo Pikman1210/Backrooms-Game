69658b395157a56e5cec1386b36b0bf3e770a45c
Pikman1210 <72507796+Pikman1210@users.noreply.github.com>
2023-04-27 21:39:15 -0500

    Endless Highscore

--- Assets/Scripts/EscapeDoorEndless.cs
+++ Assets/Scripts/EscapeDoorEndless.cs
@@ -7,17 +7,28 @@ public class EscapeDoorEndless : MonoBehaviour
     public GameObject failureUI;
     public TextMeshProUGUI timeSurvivedText;
     public TextMeshProUGUI timeSurvivedTextFailure;
+    public TextMeshProUGUI highscoreText;
+    public TextMeshProUGUI highscoreTextFailure;
 
     private float timeSurvived;
+    private float highscore;
 
     public void EndGameEndless()
     {
         FindObjectOfType<MonsterController>().DisableMonster();
 
         timeSurvived = PlayerPrefs.GetFloat("timeSurvived");
+        highscore = PlayerPrefs.GetFloat("endlessHighscore");
+
+        if (timeSurvived > highscore)
+        {
+            PlayerPrefs.SetFloat("endlessHighscore", timeSurvived);
+        }
+        highscore = PlayerPrefs.GetFloat("endlessHighscore");
 
         failureUI.SetActive(true);
         timeSurvivedTextFailure.SetText(timeSurvived.ToString());
+        highscoreTextFailure.SetText(highscore.ToString());
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
@@ -30,9 +41,17 @@ public class EscapeDoorEndless : MonoBehaviour
             FindObjectOfType<MonsterController>().DisableMonster();
 
             timeSurvived = PlayerPrefs.GetFloat("timeSurvived");
+            highscore = PlayerPrefs.GetFloat("endlessHighscore");
+
+            if (timeSurvived > highscore)
+            {
+                PlayerPrefs.SetFloat("endlessHighscore", timeSurvived);
+            }
+            highscore = PlayerPrefs.GetFloat("endlessHighscore");
 
             completionUI.SetActive(true);
             timeSurvivedText.SetText(timeSurvived.ToString());
+            highscoreText.SetText(highscore.ToString());
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
